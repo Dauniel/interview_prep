67fa661aa5243a0e801cfdd43540a01da3ea74f6
Dauniel <danieljchang5@gmail.com>
2024-09-25 15:31:27 -0700

    update

--- concepts.py
+++ concepts.py
@@ -66,7 +66,7 @@ def numIslands(self, grid: list[list[str]]) -> int:
         return 0
     
     def dfs(grid, i, j):
-        if i < 0 or i >= len(grid) or j > 0 or j >= len(grid[0]) or grid[i][j] == '0':
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
             return 
         else:
             grid[i][j] == '0' # marking it as visited
@@ -235,3 +235,41 @@ def characterReplacement(s: str, k: int) -> int:
     return max_length
 
 print(characterReplacement("AAABABB", 1))
+
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.array = []
+        self.length = 0
+
+    def get(self, i: int) -> int:
+        return self.array[i]
+
+    def set(self, i: int, n: int) -> None:
+        self.array[i] = n
+
+    def pushback(self, n: int) -> None:
+        if self.length == self.capacity:
+            self.resize()
+        self.array[self.length] = n
+        self.length += 1
+
+    def popback(self) -> int:
+        [1,2,3]
+        self.length -= 1
+        return self.array[self.length]
+
+    def resize(self) -> None:
+        self.capacity = 2 * self.capacity
+        new_array = [0] * self.capacity
+        for i in range(len(self.array)):
+            new_array[i] = self.array[i]
+        self.array = new_array
+
+    def getSize(self) -> int:
+        return self.length
+    
+    def getCapacity(self) -> int:
+        return self.capacity
+    
